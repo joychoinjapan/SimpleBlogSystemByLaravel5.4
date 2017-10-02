bf8121d22131c0142ff618590fa14f4cd2aa35f1
miaobaobao <joychoinjapan@gmail.com>
2017-10-02 18:13:54 +0900

    create migrations

---
+++ database/migrations/2017_10_01_122616_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //创建表
+        Schema::create('posts',function(Blueprint $table){
+            //自增id
+            $table->increments('id');
+            $table->string('title',100)->default('');
+            $table->text('content');
+            $table->integer('user_id')->default(0);
+            $table->timestamps();
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //回滚的命令
+        Schema::dropIfExists('posts');
+    }
+}
