6784deae967ebaf02aac4446cac92a9d9b7301c7
miaobaobao <joychoinjapan@gmail.com>
2017-10-05 21:20:56 +0900

    change js/css link and add csrf into  meta tag
    Signed-off-by: miaobaobao <joychoinjapan@gmail.com>

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -9,19 +9,16 @@
     <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
     <meta name="description" content="">
     <meta name="author" content="">
-    <meta name="csrf-token" content="MESUY3topeHgvFqsy9EcM916UWQq6khiGHM91wHy">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
     <title>laravel for blog</title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-
-
     <!-- Custom styles for this template -->
-    <link href="/resources/css/blog.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="/resources/css/wangEditor.min.css">
-
+    <link href="/css/blog.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/css/wangEditor.min.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -59,8 +56,8 @@
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="/resources/js/wangEditor.min.js"></script>
-<script src="/resources/js/ylaravel.js"></script>
+<script type="text/javascript" src="/js/wangEditor.min.js"></script>
+<script type="text/javascript" src="/js/ylaravel.js"></script>
 
 </body>
 </html>
