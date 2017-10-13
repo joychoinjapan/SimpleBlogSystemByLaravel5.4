a87e6681142df6e778cbf130a8493823a8e04bff
miaobaobao <joychoinjapan@gmail.com>
2017-10-13 22:06:46 +0900

    add login interface to those who has'nt logged in。 add account interface to logged-in users。
    Signed-off-by: miaobaobao <joychoinjapan@gmail.com>

--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -20,15 +20,23 @@
 
         <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
+                @unless(\Illuminate\Support\Facades\Auth::check())
+                    <a href="/login" class="blog-nav-item">请登录<span class="caret"></span></a>
+                @endunless
+                @if(\Illuminate\Support\Facades\Auth::check())
                 <div>
-                    <img src="/storage/9f0b0809fd136c389c20f949baae3957/iBkvipBCiX6cHitZSdTaXydpen5PBiul7yYCc88O.jpeg" alt="" class="img-rounded" style="border-radius:500px; height: 30px">
-                    <a href="#" class="blog-nav-item dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Kassandra Ankunding2  <span class="caret"></span></a>
+                    <img src="{{\Illuminate\Support\Facades\Auth::user()->avatar}}" alt="" class="img-rounded" style="border-radius:500px; height: 30px">
+                    <a href="#" class="blog-nav-item dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            {{\Illuminate\Support\Facades\Auth::user()->name}}
+                        <span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="/user/5">我的主页</a></li>
-                        <li><a href="/user/5/setting">个人设置</a></li>
+                        <li><a href="/user/me/setting">个人设置</a></li>
                         <li><a href="/logout">登出</a></li>
                     </ul>
                 </div>
+                @endif
+
             </li>
         </ul>
     </div>
