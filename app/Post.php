8c811d884d10fe010c9b2d3b77f93f2af956f6cc
miaobaobao <joychoinjapan@gmail.com>
2017-10-13 22:14:50 +0900

    link post with user
    Signed-off-by: miaobaobao <joychoinjapan@gmail.com>

--- app/Post.php
+++ app/Post.php
@@ -8,4 +8,16 @@ use App\Model;
 class Post extends Model
 {
 
+    //link with user
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+
+    }
+
+    //get comment model
+    public function comments()
+    {
+        return $this->hasMany('App\comment')->orderBy('created_at','desc');
+    }
 }
