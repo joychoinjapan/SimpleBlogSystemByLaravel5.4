ceebdfd91192f6bf454d23d1ba951181eed41c49
miaobaobao <joychoinjapan@gmail.com>
2017-10-17 11:32:26 +0900

    updates
    Signed-off-by: miaobaobao <joychoinjapan@gmail.com>

--- public/js/ylaravel.js
+++ public/js/ylaravel.js
@@ -1,6 +1,15 @@
 var editor = new wangEditor('content');
+if(editor.config){
 editor.config.uploadImgUrl='/posts/image/upload';
 editor.config.uploadHeaders = {
     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 };
 editor.create();
+}
+
+//头像预览的功能
+$(".preview_input").change(function (event) {
+    var file=event.currentTarget.files[0];
+    var url=window.URL.createObjectURL(file);
+    $(event.target).next('.preview_img').attr('src',url);
+});
