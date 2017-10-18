63e30260ef89fd76c3450597e2b2dc377ad834b2
miaobaobao <joychoinjapan@gmail.com>
2017-10-18 18:24:50 +0900

    add relation of fan module
    Signed-off-by: miaobaobao <joychoinjapan@gmail.com>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -45,7 +45,38 @@ class UserController extends Controller
         $user->save();
         return back();
 
+    }
 
 
+    //个人中心页面
+    public function show(User $user)
+    {
+        //这个人的信息：用户名，头像，关注，粉丝文章数。
+        $user=User::withCount(['stars','fans','posts'])->find($user->id);
+
+        //这个人的文章列表，取创建时间最新的前十条。
+        $posts=$user->posts()->orderBy('created_at','desc')->take(10)->get();
+
+        //这个人关注的用户，返回的是每个用户的信息。
+        $stars=$user->stars;
+        $susers=User::whereIn('id',$stars->pluck('fan_id'))->withCount(['stars','fans','posts'])->get();
+
+        //这个人的粉丝，粉丝用户的文章数。
+        $fans=$user->fans;
+        $fusers=User::whereIn('id',$fans->pluck('fan_id'))->withCount(['stars','fans','posts'])->get();
+
+        return view('user/show',compact('user','posts','susers','fusers'));
+    }
+
+    //关注用户
+    public function fan()
+    {
+        return null;
+    }
+
+    //取消关注
+    public function unfan()
+    {
+        return null;
     }
 }
