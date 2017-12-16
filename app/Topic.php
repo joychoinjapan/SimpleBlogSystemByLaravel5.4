e68eb293bd5b853a8d8c7062847f6791cc2951c5
miaobaobao <joychoinjapan@gmail.com>
2017-12-16 20:28:13 +0900

    change
    Signed-off-by: miaobaobao <joychoinjapan@gmail.com>

---
+++ app/Topic.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+use App\Model;
+class Topic extends Model
+{
+    //属于这个专题的所有文章
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class,'post_topics','topic_id','post_id');
+    }
+
+    //属于这个专题的文章数,用于withcount
+    public function postTopics()
+    {
+        return $this->hasMany(PostTopic::class,'topic_id','id');
+    }
+}
